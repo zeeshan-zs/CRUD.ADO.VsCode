a7ff0f1f1b62ce44d5a92446e6fd6e79e37ef752
Zeeshan Zubair <zeeshann.zss@gmail.com>
2019-01-22 11:23:00 +0400

    Initial Commit

---
+++ Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,130 @@
+using System;    
+using System.Collections.Generic;    
+using System.Data;    
+using System.Data.SqlClient;    
+using System.Linq;    
+using System.Threading.Tasks;
+
+namespace MvcAdoDemo.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string ConnectionString = "Data Source=(Local);Initial Catalog=EmpMgmt;Integrated Security=True";
+
+        //To View all employee details
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            List<Employee> lstemployee = new List<Employee>();
+
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployees",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    Employee employee = new Employee();
+
+                    employee.ID = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Department = rdr["Department"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    lstemployee.Add(employee);
+                }
+
+                con.Close();
+            }
+            return lstemployee;
+        }
+
+        //To Add a new employee
+
+        public void AddEmployee(Employee employee)
+        {
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployee",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name",employee.Name);
+                cmd.Parameters.AddWithValue("@Gender",employee.Gender);
+                cmd.Parameters.AddWithValue("@Department",employee.Department);
+                cmd.Parameters.AddWithValue("@City",employee.City);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //Update employee details
+
+        public void UpdateEmployee(Employee employee)
+        {
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateEmployee",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                cmd.Parameters.AddWithValue("@Name",employee.Name);
+                cmd.Parameters.AddWithValue("@Gender",employee.Gender);
+                cmd.Parameters.AddWithValue("@Department",employee.Department);
+                cmd.Parameters.AddWithValue("@City",employee.City);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //Get the details of a particular employee
+
+        public Employee GetEmployeeData(int? id)
+        {
+            Employee employee = new Employee();
+
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                string sqlQuery = "SELECT * from tblEmployee where EmployeeID =" + id;
+
+                SqlCommand cmd = new SqlCommand(sqlQuery,con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);    
+                    employee.Name = rdr["Name"].ToString();    
+                    employee.Gender = rdr["Gender"].ToString();    
+                    employee.Department = rdr["Department"].ToString();    
+                    employee.City = rdr["City"].ToString();
+                }
+
+            }
+            return employee;
+        }
+
+        //Delete a particular employee
+
+        public void DeleteEmployee(int? Id)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteEmployee",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId",Id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
